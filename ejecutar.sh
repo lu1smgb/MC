3a08968dde9f23a5829b9f2f649cc9c22437b2ba
lu1smgb <lu1smgb.gacc@gmail.com>
2022-12-16 00:13:38 +0100

    modified: ejecutar.sh

--- ejecutar.sh
+++ ejecutar.sh
@@ -1,5 +1,15 @@
 flex++ flex.l
 
+if [ $? != 0 ]; then
+    echo Error de flex, abortando...
+    exit $?
+fi
+
 g++ lex.yy.cc -lfl -o prog
+
+if [ $? != 0 ]; then
+    Error del compilador, abortando...
+    exit $?
+fi
 
 ./prog $1
